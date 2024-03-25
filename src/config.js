828f1cac08e1fa5872d638e6ea7f6a65d59ffdd0
Harsha-2324 <rharshva@gmail.com>
2024-03-25 14:26:37 +0530

    Markdown_Viewer_FE commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
   // api: "http://localhost:8000",
-  api: "https://markdown-viewer-backend.onrender.com",
+  api: "https://capstone-markdown-viewer-backend-n5eh.onrender.com",
 };
